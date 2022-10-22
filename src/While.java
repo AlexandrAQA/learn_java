9a1bc64c5eb2aa28ad533f884e117e395a21d35b
Alex <panaev.as@gmail.com>
2022-10-22 14:04:40 +0400

    3rd branch commit, continue working with cycles

--- src/While.java
+++ src/While.java
@@ -1,23 +1,29 @@
 import java.util.Scanner;
 
 public class While {
-    public static void main (String[] args) throws java.lang.Exception
-    {
+    public static void main(String[] args) {
         // simple cycle
         int x = 10;
-        while (x > 8 ) {
+        while (x > 8) {
             System.out.println("Hello from cycle");
             x--;
         }
 
-        //while user print only numbers the cycle is running
-            Scanner console = new Scanner(System.in);
-            while(console.hasNextInt())
-            {
-                int y = console.nextInt();
-            }
-
+        Scanner scanner = new Scanner(System.in);
+        int i = 0;
+        while (i < 5) {
+            System.out.println(i);
+            i++;
 
         }
-    }
 
+        //while user print only numbers the cycle is running
+        System.out.println("Please enter any Integer");
+        Scanner console = new Scanner(System.in);
+        while (console.hasNextInt()) {
+            int y = console.nextInt();
+        }
+
+
+    }
+}
